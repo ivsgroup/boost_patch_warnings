c75f766b753d548049ff0095f2abe453758cb0ba
Pascal Thomet <pthomet@ivsweb.com>
2017-07-08 12:08:02 +0200

    Add ../make_boost_1_64_patched.sh

---
+++ make_boost_1_64_patched.sh
@@ -0,0 +1,11 @@
+if [ ! -d work_folder ]; then
+  mkdir work_folder
+fi
+cd work_folder
+
+wget https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2
+tar xvf boost_1_64_0.tar.bz2
+mv boost_1_64_0 boost_1_64_0-patchivs
+cd boost_1_64_0-patchivs
+patch -p1 -i  ../../boost_patch_ivs.patch
+
